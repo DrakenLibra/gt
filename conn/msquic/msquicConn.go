c2786e49706e8cb29ebc16c5f3c055488fba7c6b
Shadow <zhhr@bupt.edu.cn>
2023-10-27 21:17:56 +0800

    Try to restructure msquic-gt

--- conn/msquic/msquicConn.go
+++ conn/msquic/msquicConn.go
@@ -11,12 +11,14 @@ type MsquicConn struct {
 }
 
 func (q *MsquicConn) Close() (err error) {
-	err1 := q.Conn.Close()
-	err2 := q.Parent.Close()
-	if err1 != nil {
-		return err1
-	}
-	return err2
+	//err1 := q.Conn.Close()
+	//err2 := q.Parent.Close()
+	//if err1 != nil {
+	//	return err1
+	//}
+	//return err2
+	err = q.Conn.Close()
+	return
 }
 
 func MsquicDial(addr string, config *tls.Config) (conn net.Conn, err error) {
